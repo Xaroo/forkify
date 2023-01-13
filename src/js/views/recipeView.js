66afc519c0aacdf58a12712235bd19609704a3e3
Xaroo <pbujas0@wp.pl>
2023-01-13 20:32:46 +0100

    Fractions added

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -124,7 +124,9 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${el.quantity ? el.quantity : ''}</div>
+      <div class="recipe__quantity">${
+        el.quantity ? numberToFraction(el.quantity).toString() : ''
+      }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${el.unit}</span>
         ${el.description}
